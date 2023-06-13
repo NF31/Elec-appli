affbc740daae7dc8581a155c9f1d4a0fbba42e9c
NF <jeaneudes.woto31@gmail.com>
2023-06-13 21:55:34 +0200

    maj

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,10 @@ function openPopup(){
 function closePopup(){
     popup.classList.remove("open-popup");
 }
+
+const menu = document.querySelector(".menu-hamburger");
+const nav = document.querySelector (".nav-links");
+
+menu.addEventListener('click', () =>{
+    nav.classList.toggle('mobile-menu')
+})
