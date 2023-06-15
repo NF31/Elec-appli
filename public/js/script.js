974fe499a91296ff8ff6ea3569c4ace8e95e3b4d
NF <jeaneudes.woto31@gmail.com>
2023-06-16 00:11:03 +0200

    maj

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,11 @@ const nav = document.querySelector (".nav-links");
 menu.addEventListener('click', () =>{
     nav.classList.toggle('mobile-menu')
 })
+const body = document.querySelector('body');
+const p = document.querySelector('pareil');
+const toggle = document.getElementById('toggle');
+toggle.addEventListener ('click', () =>{
+    body.classList.toggle('active')
+    p.classList.toggle('active')
+    toggle.classList.toggle('active')
+}) 
