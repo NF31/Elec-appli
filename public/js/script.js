023517877a72a41aac03baf174b4075165db76f4
NF <jeaneudes.woto31@gmail.com>
2023-06-17 18:36:29 +0200

    maj

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,9 @@
-let popup = document.getElementById("popup");
 
-function openPopup(){
-    popup.classList.add("open-popup");
+
+let icon = document.getElementById("icon");
+let p = document.querySelector('a')
+let body = document.querySelector('body')
+icon.onclick = function() {
+   body.classList.toggle('dark')
+   p.classList.toggle('dark')
 }
-
-function closePopup(){
-    popup.classList.remove("open-popup");
-}
-
-const menu = document.querySelector(".menu-hamburger");
-const nav = document.querySelector (".nav-links");
-
-menu.addEventListener('click', () =>{
-    nav.classList.toggle('mobile-menu')
-})
-const body = document.querySelector('body');
-const p = document.querySelector('pareil');
-const toggle = document.getElementById('toggle');
-toggle.addEventListener ('click', () =>{
-    body.classList.toggle('active')
-    p.classList.toggle('active')
-    toggle.classList.toggle('active')
-}) 
