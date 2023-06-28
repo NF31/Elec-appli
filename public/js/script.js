f0fc052c8aa687989c47dca13641c96222a85eed
NF <jeaneudes.woto31@gmail.com>
2023-06-28 02:12:15 +0200

    maj

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-let icon = document.getElementById("icon");
-let p = document.querySelector('.pareil');
-let body = document.querySelector('body');
-icon.onclick = function() {
-   body.classList.toggle('dark')
-   p.classList.toggle('dark')
-}
